307e279d9cbf93d6137953565906160461e29d2d
Ty Lewis <tyglewis@gmail.com>
2014-02-17 22:27:25 -0700

    Got circles working.

--- CS355/src/cs355/solution/MouseCircleBuilderStrategy.java
+++ CS355/src/cs355/solution/MouseCircleBuilderStrategy.java
@@ -23,8 +23,8 @@ public class MouseCircleBuilderStrategy extends MouseShapeBuilderStrategy
     @Override
     public void mousePressed(MouseEvent mouseEvent)
     {
-         _p0 = mouseEvent.getPoint();
-         _p1 = _p0;
+        _p0 = WorldToScreen.getInstance().getInWorldCoords(mouseEvent.getPoint());
+        _p1 = _p0;
         Circle square = new Circle(new Vector2D(_p0), 0, cs355.model.Context.getInstance().getCurrentColor());
         Canvas.getInstance().addShape(square);
     }
@@ -32,7 +32,7 @@ public class MouseCircleBuilderStrategy extends MouseShapeBuilderStrategy
     @Override
     public void mouseDragged(MouseEvent mouseEvent)
     {
-        _p1 = mouseEvent.getPoint();
+        _p1 = WorldToScreen.getInstance().getInWorldCoords(mouseEvent.getPoint());
         double width = _p1.getX() - _p0.getX();
         double height = _p1.getY() - _p0.getY();
 
